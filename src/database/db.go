a2743035221a949f56290db028d923d0a8e6fea7
Aleksandar Markovic <aleksandar.markovic@firstbeatmedia.com>
2021-10-29 19:54:13 +0200

    setup microservices

--- src/database/db.go
+++ src/database/db.go
@@ -2,7 +2,6 @@ package database
 
 import (
 	"github.com/aleksbgs/ambassador/src/models"
-	"github.com/aleksbgs/ambassador/utils"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -11,12 +10,15 @@ var DB *gorm.DB
 
 func Connect() {
 	var err error
-	host := utils.ViperEnvVariable("DBDOCKERHOST")
-	dbuser := utils.ViperEnvVariable("DBUSER")
-	password := utils.ViperEnvVariable("DBPASSWORD")
-	dbname := utils.ViperEnvVariable("DBNAME")
+	//host := utils.ViperEnvVariable("DBDOCKERHOST")
+	//dbuser := utils.ViperEnvVariable("DBUSER")
+	//password := utils.ViperEnvVariable("DBPASSWORD")
+	//dbname := utils.ViperEnvVariable("DBNAME")
+    //microservice setup
+	 dsn := "host=" + "db" + " " + "user=" + "root" + " " + "password=" + "root" + " " + "dbname=" + "ambassador" + " " + "port=5432 sslmode=disable"
+    //local setup
+	//dsn := "host=" + "localhost" + " " + "user=" + "root" + " " + "password=" + "root" + " " + "dbname=" + "ambassador" + " " + "port=5432 sslmode=disable"
 
-	dsn := "host=" + host + " " + "user=" + dbuser + " " + "password=" + password + " " + "dbname=" + dbname + " " + "port=5432 sslmode=disable"
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
